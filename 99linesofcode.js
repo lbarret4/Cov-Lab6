e1d114b87353421062aa7627d7467ee7350da4a8
lbarret4 <llewellynbar4@gmail.com>
2018-08-18 21:57:32 -0400

    Created 99linesofcode file and  createSong function

---
+++ 99linesofcode.js
@@ -0,0 +1,16 @@
+let friends = ['Charles','Jemma','Kim','Amanda','Kenji'];
+
+
+function createSong(name){
+    for (i= 99;i > 0;i--){
+        if(i == 1){
+
+        } else if( i== 99) {
+            console.log(`${name}:`);
+            console.log(i+'lines of code in the file,'+i+`lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file`);
+        }
+        else
+        
+    }
+
+}

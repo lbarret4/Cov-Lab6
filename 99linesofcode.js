82939133728e2c26fa33d9d374d4bec073db5826
lbarret4 <llewellynbar4@gmail.com>
2018-08-18 22:11:17 -0400

    Change if statements to switch statements.

--- 99linesofcode.js
+++ 99linesofcode.js
@@ -1,15 +1,20 @@
 let friends = ['Charles','Jemma','Kim','Amanda','Kenji'];
 
-
+for (friend of friends ){
+    createSong(friend);
+}
 function createSong(name){
     for (i= 99;i > 0;i--){
-        if(i == 1){
-
-        } else if( i== 99) {
-            console.log(`${name}:`);
-            console.log(i+'lines of code in the file,'+i+`lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file`);
+        switch(i){
+            case 1:
+                console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, no more lines of code in the file`);
+                break;
+            case 99:
+                console.log(`${name}:`);
+            default:
+                console.log(i+' lines of code in the file,'+i+` lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file`);
         }
-        else
+      
         
     }
 

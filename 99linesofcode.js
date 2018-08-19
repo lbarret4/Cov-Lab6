72e88c14f19ea6dff446ecc6dd49416e2c4931bd
lbarret4 <llewellynbar4@gmail.com>
2018-08-18 22:58:38 -0400

    Added style files

--- 99linesofcode.js
+++ 99linesofcode.js
@@ -1,21 +1,21 @@
-let friends = ['Charles','Jemma','Kim','Amanda','Kenji'];
+let friends = ['Charles', 'Jemma', 'Kim', 'Amanda', 'Kenji'];
 
-for (friend of friends ){
+for (friend of friends) {
     createSong(friend);
 }
-function createSong(name){
-    for (i= 99;i > 0;i--){
-        switch(i){
+function createSong(name) {
+    for (i = 99; i > 0; i--) {
+        switch (i) {
             case 1:
                 console.log(`${i} lines of code in the file, ${i} lines of code; ${name} strikes one out, clears it all out, no more lines of code in the file`);
                 break;
             case 99:
                 console.log(`${name}:`);
             default:
-                console.log(i+' lines of code in the file,'+i+` lines of code; ${name} strikes one out, clears it all out, ${i-1} lines of code in the file`);
+                console.log(i + ' lines of code in the file,' + i + ` lines of code; ${name} strikes one out, clears it all out, ${i - 1} lines of code in the file`);
         }
-      
-        
+
+
     }
 
 }
